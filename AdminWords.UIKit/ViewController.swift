902955ea18b6eb653d2a0db37d850c3a16377728
Yevhenii <jksemenyuk@gmail.com>
2022-09-03 16:58:40 +0200

    Initial Commit

---
+++ AdminWords.UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdminWords.UIKit
+//
+//  Created by 111 on 03.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
